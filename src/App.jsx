b13cc8ca4d8244f9209e654d7f35b895148f4bff
Mohamed-khaled77 <mohamedkahled.0345@gimal.com>
2024-10-10 20:58:03 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Regetar from './login page/regestar'
+export default function App() {
+  return (
+    <div className=' shadow-box   px-0 col-12 wed'>
+      <Regetar/>
+    </div>
+  )
+}
+
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+createRoot(document.getElementById('root')).render(
+
+    <App />
+
+)
