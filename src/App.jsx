d514afb1869c993c633df91353f3efadf3d845c0
Dabo-2100 <a_fattah_m@icloud.com>
2024-10-11 10:48:04 +0300

    Dabo Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Regetar from './login page/regestar'
 export default function App() {
   return (
     <div className=' shadow-box   px-0 col-12 wed'>
-      <Regetar/>
-      <App/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/register" element={<Regetar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
